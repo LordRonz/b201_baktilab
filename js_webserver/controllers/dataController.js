c919d1fba591e3fff903735e45d52e87070d1ee9
LordRonz <aarontanhar2000@gmail.com>
2021-03-21 10:07:17 +0700

    PUT updated

--- js_webserver/controllers/dataController.js
+++ js_webserver/controllers/dataController.js
@@ -12,6 +12,8 @@ async function getAllData(req, res) {
         res.end();
     } catch(error) {
         console.log(error);
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        return res.end({ message: "Internal Server Error" });
     }
 }
 
@@ -32,6 +34,8 @@ async function getData(req, res, id) {
         }
     } catch(error) {
         console.log(error);
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        return res.end({ message: "Internal Server Error" });
     }
 }
 
@@ -41,40 +45,46 @@ async function createData(req, res) {
         const data = JSON.parse(body);
         const newData = await Data.create(data);
         res.writeHead(201, { 'Content-Type': 'application/json' });
-            return res.end(JSON.stringify(newData));
+        return res.end(JSON.stringify(newData));
     } catch(error) {
         console.log(error);
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        return res.end({ message: "Internal Server Error" });
     }
 }
 
 async function updateData(req, res, id) {
     try {
-        const data = await Data.findById(id);
+        const body = await getPostData(req);
+        const filter = { _id: ObjectId(id) };
+        const updateDoc = {
+            $set: JSON.parse(body),
+        };
+        const updData = await Data.update(filter, updateDoc);
     
-        if(!data) {
-            res.statusCode = 404;
-            res.setHeader('Content-Type', 'application/json');
-            res.write(JSON.stringify({ message: 'Data Not Found' }));
-            res.end();
-        }
-        else {
-            const body = await getPostData(req);
-            const { title, description, price } = JSON.parse(body);
-            const filter = { _id: ObjectId(id) };
-            const updateDoc = {
-                $set: JSON.parse(body),
-            };
+        if(updData.modifiedCount === 1) {
             // const dataData = {
             //     title: title || product.title,
             //     description: description || product.description,
             //     price: price || product.price,
             // };
-            const updData = await Data.update(filter, updateDoc);
             res.writeHead(200, { 'Content-Type': 'application/json' });
             return res.end(JSON.stringify(updData));
         }
+        else if(updData.matchedCount === 1){
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.write(JSON.stringify({ message: 'Data Found But Not Modified' }));
+            return res.end();
+        }
+        else {
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            return res.end({ message: "Data Not Found" });
+        }
     } catch(error) {
         console.log(error);
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        return res.end({ message: "Internal Server Error" });
     }
 }
 
